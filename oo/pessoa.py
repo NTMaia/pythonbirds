c3f01d3223dd7a513c84f74493a7fe1829945f9e
Natan <natanmaia02@gmail.com>
2020-08-25 15:47:20 -0300

    Criados atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,18 +1,19 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=35):
-        self.idade = idade
+    def __init__(self, *filhos, nome=None, idade=35):
+        self.idade = f'{idade} anos'
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
-        return f'Olá {id(self)}'
+        return f'Olá {(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa()
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Natan'
-    print(p.nome)
-    print(p.idade)
+    natan = Pessoa(nome ='Natan')
+    maia = Pessoa(natan, nome =  'Maia')
+    print(Pessoa.cumprimentar('Natan'))
+    print(id(maia))
+    print(maia.cumprimentar())
+    print(natan.idade)
+    for filho in maia.filhos:
+        print(filho.nome)
